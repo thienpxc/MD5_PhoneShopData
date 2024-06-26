115810fdbb6cecde050656c4c29f832352be457f
thienpxc <thienpxc@gmail.com>
2024-06-26 17:31:53 +0700

    java

---
+++ src/main/java/com/example/md5_phoneshopdata/Md5PhoneShopDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.md5_phoneshopdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md5PhoneShopDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md5PhoneShopDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/md5_phoneshopdata/Md5PhoneShopDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.md5_phoneshopdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md5PhoneShopDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
